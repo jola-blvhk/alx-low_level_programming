a8321eacfefa6028327ed6d49ab499ebf8187c3b
Oluremi Bello <awujoolabello@gmail.com>
2022-03-15 03:46:05 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+printf("%d", i);
+}
+
+printf("\n");
+
+return (0);
+}
