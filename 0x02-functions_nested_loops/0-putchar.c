643b4582b18370526ee00e66f7ba11be2201301b
Oluremi Bello <awujoolabello@gmail.com>
2022-03-15 15:09:19 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,24 @@
  * Return 0 (success)
  */
 
+void print_putchar(void);
+
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+	print_putchar();
+	return(0);
+}
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+void print_putchar(void);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
-	return (0);
 
 }
