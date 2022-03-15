3af5da386c50c22a271b761b7b7e514a68a6976a
Oluremi Bello <awujoolabello@gmail.com>
2022-03-14 17:01:16 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar("\n");
+
+return (0)
+}
