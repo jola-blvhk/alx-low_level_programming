7fddbecaf6be2787ee7b3e7ad01051551756cc27
Oluremi Bello <awujoolabello@gmail.com>
2022-03-15 16:05:31 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet lowercase
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+
+}
