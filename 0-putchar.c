2ea91223f92ea1955f5aa1c7c3ffad78eb314b45
Oluremi Bello <awujoolabello@gmail.com>
2022-03-15 10:02:07 -0700

    task 0

---
+++ 0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: always 0 (success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 57
