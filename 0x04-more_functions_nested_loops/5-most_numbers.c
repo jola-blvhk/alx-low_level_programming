27840c539423199e55b099c27566c4ad6828ce70
Oluremi Bello <awujoolabello@gmail.com>
2022-03-17 18:31:17 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-most_numbers.c
+++ 0x04-more_functions_nested_loops/5-most_numbers.c
@@ -1,24 +1,49 @@
 #include "main.h"
 /**
- * print_most_numbers - print numbers
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  *
  * Return: Always 0.
  */
 
-void print_most_numbers(void)
+void more_numbers(void)
 {
 	int i;
+	int first_number;
+	int second_number;
+	int result;
 
 	i = 0;
+	result = 0;
 	while (i < 10)
 	{
-		if (i != 2 && i != 4)
+		while (result <= 14)
 		{
-			_putchar (i + '0');
+			if (result < 10)
+			{
+				second_number = result;
+			}
+
+			else
+			{
+				first_number = result / 10;
+				second_number = result % 10;
+				_putchar (first_number + '0');
+			}
+
+			_putchar (second_number + '0');
+
+			result++;
+
 		}
+
 		i++;
+
+		result = 0;
+
+		_putchar ('\n');
+
 	}
 
-	_putchar ('\n');
+
 
 }
