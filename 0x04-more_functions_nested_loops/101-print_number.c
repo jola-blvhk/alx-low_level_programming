78e955047302231e551d03de0582cdfc88f2894b
Oluremi Bello <awujoolabello@gmail.com>
2022-03-17 20:16:01 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,17 @@
 
 void print_number(int n)
 {
-
 		unsigned int x = n;
 
 		if (n < 0)
 		{
 			_putchar(45);
 			x = -x;
-		}	
+		}
 		if ((x / 10) > 0)
 		{
 			print_number(x / 10);
-
 		}
+
 		_putchar((x % 10) + 48);
 }
