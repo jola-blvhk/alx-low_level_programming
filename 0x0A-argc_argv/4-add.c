2e292d2816245cbfe2964b34552f2f21a505bde1
Oluremi Bello <awujoolabello@gmail.com>
2022-04-03 02:25:06 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,9 +4,10 @@
 #include <string.h>
 
 /**
- * check_num - check - string there are digit
+ * check_num - string there are digit.
  * @str: array str
- * Return; Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 
 int check_num(char *str)
@@ -35,7 +36,8 @@ int check_num(char *str)
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- * return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
@@ -43,13 +45,13 @@ int main(int argc, char *argv[])
 	/*Declaring variables*/
 	int count;
 	int str_to_int;
-
 	int sum = 0;
+
 	count = 1;
 
 	while (count < argc) /*Goes through the whole array*/
 	{
-		if(check_num(argv[count]))
+		if (check_num(argv[count]))
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
